a7f73ab56377b330bf4febfc6602871ee95d3840
anushikhalaad <121476570+Anushikhalaad@users.noreply.github.com>
2026-07-15 23:26:57 +0530

    use effect

--- src/Fom/Form.js
+++ src/Fom/Form.js
@@ -1,35 +1,42 @@
 import './Form.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function MyForm({ handleFormSubmit, value = '', errorMsg }) {
-    const [localTask, setLocalTask] = useState(value);
+  const [localTask, setLocalTask] = useState(value);
+  const inputRef = useRef(null);
 
 
-    const handleChange = (event) => {
-        setLocalTask(event.target.value); // asynchronous update of the state
-    }
+  const handleChange = (event) => {
+    setLocalTask(event.target.value); // asynchronous update of the state
+  }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        handleFormSubmit(localTask);
-        setLocalTask(''); // Clear the input field after submission
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    handleFormSubmit(localTask);
+    setLocalTask(''); // Clear the input field after submission
+  }
 
-    useEffect(() => {
-        document.getElementById('input').focus();
-    }, [])
+  useEffect(() => {
+    console.log('useEffect Form component mounted');
+    inputRef.current.focus();
+  }, [])
 
+  useEffect(() => {
+    console.log('useEffect: on monting and update', localTask);
+  }, [localTask])
 
-    return (
-        <form className="form" onSubmit={handleSubmit}>
-            <input id="input" className="Input" type="text"
-                style={errorMsg ? { border: '5px solid red' } : {}}
-                placeholder="Enter a new task..." name="taskInput"
-                value={localTask}
-                onChange={handleChange} />
-            {value ?
-                <button className='Button' type="submit">Edit Task</button> :
-                <button className="Button" type="submit" disabled={localTask === ""}>Add Task</button>}
-        </form>
-    );
+  console.log("JSX localTask", localTask)
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <input ref={inputRef} className="Input" type="text"
+        style={errorMsg ? { border: '5px solid red' } : {}}
+        placeholder="Enter a new task..." name="taskInput"
+        value={localTask}
+        onChange={handleChange} />
+      {value ?
+        <button className='Button' type="submit">Edit Task</button> :
+        <button className="Button" type="submit" disabled={localTask === ""}>Add Task</button>}
+    </form>
+  );
 }
